8a4e29be885b1ffe43cc165d356534ffd8a60867
KryDos <furyinbox@gmail.com>
2015-01-04 16:00:11 +0300

    server do not kill client connection when message to the client is sent

--- src/test_clj/core.clj
+++ src/test_clj/core.clj
@@ -1,53 +1,25 @@
 (ns test-clj.core
   (:gen-class)
-  (:require [clojure.java.io :as io])
-  (:import [java.net ServerSocket]))
+  (:use server.socket))
+(import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
 
-;; receives messsage
-;; from the socket
-(defn receive-msg
-  [socket]
-  (.readLine (io/reader socket)))
+(def port 8888)
 
-;; send message
-;; to the socket
-(defn send-msg
-  [socket msg]
-  (let [writer (io/writer socket)]
-    (.write writer msg)
-    (.flush writer)))
+(defn process-message
+  [msg]
+  (str "Message => " msg))
 
-;; listen for connections
-;; on [port]
-(defn serv
-  [port handler]
-  (with-open [server-sock (ServerSocket. port)
-              sock (.accept server-sock)]
-    (let [msg-in (receive-msg sock)
-          msg-out (handler msg-in)]
-      (send-msg sock msg-out))))
+(defn echo-server
+  [in out]
+  (binding [*in* (BufferedReader. (InputStreamReader. in))
+            *out* (OutputStreamWriter. out)]
+    (loop []
+      (println (process-message (read-line)))
+      (recur))))
 
-;; listen for connections
-;; and continue work of the server
-(defn serv-always
-  [port handler]
-  (let [running (atom true)]
-    (future
-      (with-open [server-sock (ServerSocket. port)]
-        (while @running
-          (with-open [sock (.accept server-sock)]
-            (let [msg-in (receive-msg sock)
-                  msg-out (handler msg-in)]
-              (send-msg sock msg-out))))))
-    running))
-
-
-(defn just-print
-  [what-to-print]
-  (str "Message => " what-to-print))
 
 (defn -main
   []
-  (serv-always 8888 #(just-print %1)))
+  (create-server port echo-server))
 
 
