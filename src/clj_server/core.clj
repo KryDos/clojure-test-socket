6b24858f287b433216bae573c2338e0083e745fb
KryDos <furyinbox@gmail.com>
2015-01-18 16:31:51 +0300

    remove commented line

--- src/clj_server/core.clj
+++ src/clj_server/core.clj
@@ -1,12 +1,10 @@
 (ns clj-server.core
   (:gen-class)
   (:use server.socket)
-;;  (:require [clojure.data.json :as json])
   (:require [clj-server.message-handler :as mh]))
 (import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
 
 (def port 8888)
-(def connections-map (agent {}))
 
 (defn echo-server
   [in out]
