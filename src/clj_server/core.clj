04a241921d7ab7f1189d4c460bafa7a3a9092166
KryDos <furyinbox@gmail.com>
2015-01-18 16:31:27 +0300

    created new module. moved almost all functions to it

--- src/clj_server/core.clj
+++ src/clj_server/core.clj
@@ -1,30 +1,12 @@
 (ns clj-server.core
   (:gen-class)
   (:use server.socket)
-  (:require [clojure.data.json :as json]))
+;;  (:require [clojure.data.json :as json])
+  (:require [clj-server.message-handler :as mh]))
 (import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
 
 (def port 8888)
 (def connections-map (agent {}))
-
-(defn get-connections-list-as-json
-  "returns map-to-json" 
-  [map]
-  (json/write-str @connections-map))
-
-(defn update-connection-map
-  [map k v]
-  (send map #(assoc % k v)))
-
-(defn process-message
-  [msg]
-  (let [message-map (json/read-str msg)]
-    (if (= (get message-map "command")
-           "show")
-      (get-connections-list-as-json @connections-map)
-      (do
-        (update-connection-map connections-map (get message-map "id") (get message-map "command"))
-        (str (get message-map "command"))))))
 
 (defn echo-server
   [in out]
@@ -32,11 +14,9 @@
             *out* (OutputStreamWriter. out)]
     (loop []
       (let [message (read-line)]
-        (println (process-message message)))
+        (println (mh/process-message message)))
       (recur))))
 
 (defn -main
   []
   (create-server port echo-server))
-
-
